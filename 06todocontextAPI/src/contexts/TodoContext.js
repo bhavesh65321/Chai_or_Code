955004765cbc9223feafd9fb2abf90cfc77dfaca
bhavesh <bhavesh65321@gmail.com>
2024-03-11 12:58:56 +0530

    code added for todoitem project

---
+++ 06todocontextAPI/src/contexts/TodoContext.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { createContext,useContext } from "react"
+
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Test name",
+            completed: false,
+        }
+    ],
+    addTodo :(todo)=>{},
+    updateTodo :(id,todo) => {},
+    deleteTodo :(id) => {},
+    toggleComplete :(id) => {},
+})
+
+export const useTodo=()=>
+{
+    return useContext(TodoContext)
+}
+
+export const Todoprovider = TodoContext.Provider
+
